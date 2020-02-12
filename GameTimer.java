052b4a4a39999620c7a6e2b286e49922c20846c6
es <StaNC2000>
2020-02-12 20:49:31 +0100

    simple timer added

---
+++ GameTimer.java
@@ -0,0 +1,26 @@
+import java.awt.Toolkit;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameTimer{
+
+    Toolkit toolkit;
+
+    Timer timer;
+
+    public GameTimer(int seconds) {
+        toolkit = Toolkit.getDefaultToolkit();
+        timer = new Timer();
+        timer.schedule(new RemindTask(), seconds * 1000);
+    }
+
+    class RemindTask extends TimerTask {
+
+        @Override
+        public void run() {
+            System.out.println("Time is up !");
+            toolkit.beep();
+            System.exit(0);
+        }
+    }
+}
