4edcf5b3c3535a085b3221d9a90bfc13023118d4
KozubIgn <ignacykozub@gmail.com>
2020-02-13 21:08:39 +0100

    added score file

---
+++ Score.java
@@ -0,0 +1,11 @@
+
+
+public class Score {
+    public String name;
+    public int points;
+
+    public Score(String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+}
